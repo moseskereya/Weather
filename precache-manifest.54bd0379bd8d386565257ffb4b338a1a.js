0d874fa8878897e7d52e5be4674b83231c257044
Moses <lazaromoses77@gmail.com>
2020-05-06 20:28:36 +0300

    Updates

---
+++ precache-manifest.54bd0379bd8d386565257ffb4b338a1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d013ba315e121c72d0abdbdcc42675",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "6d9e17b038ee6e67bbe6",
+    "url": "/Weather/static/css/main.c11894d6.chunk.css"
+  },
+  {
+    "revision": "232eac584c8eeb1937fc",
+    "url": "/Weather/static/js/2.bba53bce.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Weather/static/js/2.bba53bce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d9e17b038ee6e67bbe6",
+    "url": "/Weather/static/js/main.5fc89b9d.chunk.js"
+  },
+  {
+    "revision": "b32f280c7a3d374b14ca",
+    "url": "/Weather/static/js/runtime-main.1a64d9ab.js"
+  },
+  {
+    "revision": "3e0181421833f5a5a4cb7acf30d0631c",
+    "url": "/Weather/static/media/2850815.3e018142.jpg"
+  }
+]);
